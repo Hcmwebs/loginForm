f382ff469ac03447f058d2c0c47ec2970e6e574e
Hezekia Chege Mungai <64218603+Hcmwebs@users.noreply.github.com>
2021-09-01 23:38:09 +1200

    added some styles to the form
    Signed-off-by: Hezekia Chege Mungai <64218603+Hcmwebs@users.noreply.github.com>

--- gulpfile.js
+++ gulpfile.js
@@ -47,9 +47,12 @@ function cacheBustTask() {
 
 // Watch task
 function watchTask() {
-  watch([files.scssPath, files.jsPath], parallel(scssTask, jsTask));
+  watch(
+    [files.htmlPath, files.scssPath, files.jsPath],
+    parallel(scssTask, jsTask, cacheBustTask)
+  );
 }
 
 // Default task
 
-exports.default = series(parallel(scssTask, jsTask), cacheBustTask, watchTask);
+exports.default = series(parallel(scssTask, jsTask, cacheBustTask), watchTask);
